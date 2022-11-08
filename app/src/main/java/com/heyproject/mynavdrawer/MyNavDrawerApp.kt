d13475b71fe55fe7cf6e8258fa06231c7918390e
yayanrw <yayanraw@gmail.com>
2022-11-08 13:27:59 +0700

    navbar drawer

--- app/src/main/java/com/heyproject/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/heyproject/mynavdrawer/MyNavDrawerApp.kt
@@ -1,6 +1,15 @@
 package com.heyproject.mynavdrawer
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.heyproject.mynavdrawer.ui.theme.MyNavDrawerTheme
 
@@ -11,7 +20,39 @@ Github : https://github.com/yayanrw
 
 @Composable
 fun MyNavDrawerApp() {
+    Scaffold(topBar = {
+        MyTopBar(onMenuClick = {
 
+        })
+    }) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(stringResource(R.string.hello_world))
+        }
+    }
+}
+
+@Composable
+fun MyTopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {
+                onMenuClick()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        title = {
+            Text(stringResource(R.string.app_name))
+        },
+    )
 }
 
 @Preview(showBackground = true)
