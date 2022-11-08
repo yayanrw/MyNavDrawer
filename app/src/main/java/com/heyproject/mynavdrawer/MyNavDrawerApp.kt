9bac054bab5a352bd5e0f34827bdb3f4f9da9d98
yayanrw <yayanraw@gmail.com>
2022-11-08 14:05:54 +0700

    show action

--- app/src/main/java/com/heyproject/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/heyproject/mynavdrawer/MyNavDrawerApp.kt
@@ -1,5 +1,6 @@
 package com.heyproject.mynavdrawer
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -44,10 +45,17 @@ fun MyNavDrawerApp() {
             onItemSelected = { title ->
                 scope.launch {
                     scaffoldState.drawerState.close()
-                    scaffoldState.snackbarHostState.showSnackbar(
+                    val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
                         message = context.resources.getString(R.string.coming_soon, title),
                         actionLabel = context.resources.getString(R.string.subscribe_question)
                     )
+                    if (snackbarResult == SnackbarResult.ActionPerformed) {
+                        Toast.makeText(
+                            context,
+                            context.resources.getString(R.string.subscribed_info),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             },
         )
