3431a295f62ab9bcbf69b9eae9026001557259b7
yayanrw <yayanraw@gmail.com>
2022-11-08 13:18:45 +0700

    Initial commit

---
+++ app/src/main/java/com/heyproject/mynavdrawer/MyNavDrawerApp.kt
@@ -0,0 +1,23 @@
+package com.heyproject.mynavdrawer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.heyproject.mynavdrawer.ui.theme.MyNavDrawerTheme
+
+/**
+Written by Yayan Rahmat Wijaya on 11/8/2022 13:16
+Github : https://github.com/yayanrw
+ **/
+
+@Composable
+fun MyNavDrawerApp() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyNavDrawerTheme {
+        MyNavDrawerApp()
+    }
+}
